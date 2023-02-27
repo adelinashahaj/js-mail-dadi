150749a80ed00179c34e6e7733697ace66f2695d
adelinashahaj <adelinarucaj@icloud.com>
2023-02-27 14:06:59 +0100

    inizio esercizio email

---
+++ email/js/main.js
@@ -0,0 +1,7 @@
+
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente),
+//stampa un messaggio appropriato sull’esito del controllo.
+
+
+let userdom = parseInt(prompt("Dimmi la tua email"));
